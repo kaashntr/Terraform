af73049d5f9e368ca3f58fb80e4a7b9f62382e69
maar <kovalbogdan73@gmail.com>
2025-04-26 12:21:01 +0300

    launch template added

--- launch_template.tf
+++ launch_template.tf
@@ -2,15 +2,13 @@ data "aws_ssm_parameter" "ubuntu_ami" {
   name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 resource "aws_launch_template" "launch_template" {
-  name_prefix   = "launch_template"
   image_id      = aws_ssm_parameter.ubuntu_ami.value
   instance_type = "t2.micro"
 
-  key_name = "your-key-name" # Replace with your actual key pair name
-
+  key_name = aws_key_pair.public-key.key_name
   network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = ["sg-xxxxxxxx"] # Replace with your security group
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.private.id]
   }
 
   tag_specifications {
