42d4fe398bc07b6845185dd5d7725c02f14cfb7b
maar <kovalbogdan73@gmail.com>
2025-04-27 13:22:30 +0300

    launch-temolate update

--- launch-template.tf
+++ launch-template.tf
@@ -7,7 +7,15 @@ resource "aws_launch_template" "launch_template" {
     associate_public_ip_address = false
     security_groups             = [aws_security_group.private.id]
   }
-
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    apt-get -y update
+    apt-get -y install nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "Hello from ASG instance!" > /var/www/html/index.html
+    EOF
+  )
   tag_specifications {
     resource_type = "instance"
 
