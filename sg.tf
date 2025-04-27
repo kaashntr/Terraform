d05a289d4d3d128640419468e157aba85e6f98da
maar <kovalbogdan73@gmail.com>
2025-04-28 00:20:14 +0300

    Some preparations before asg

--- sg.tf
+++ sg.tf
@@ -23,3 +23,21 @@ resource "aws_security_group" "lt-sg" {
   }
   
 }
+resource "aws_security_group" "elb-sg" {
+  vpc_id = aws_vpc.terra-demo.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0/0"]
+    description = "HTTP access from public internet"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+  
+}
