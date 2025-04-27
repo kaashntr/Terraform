5459240003217e136445c38aeaf06db5cfe59e43
maar <kovalbogdan73@gmail.com>
2025-04-28 00:04:48 +0300

    Few changes

--- sg.tf
+++ sg.tf
@@ -1,7 +1,25 @@
 resource "aws_security_group" "lt-sg" {
+  vpc_id = aws_vpc.terra-demo.id
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [""]}
+    cidr_blocks = [aws_subnet.terra-demo-private-subnet.cidr_block]
+    description = "SSH access from private subnet"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.terra-demo-private-subnet.cidr_block]
+    description = "HTTP access from private subnet"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+  
 }
--- terraform.tf
+++ terraform.tf
@@ -14,3 +14,17 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "public-key" {
+  key_name   = "public-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+resource "local_file" "private_key" {
+  content  = tls_private_key.key.private_key_pem
+  filename = "${path.module}/key.pem"
+  file_permission = "0600"
+}
