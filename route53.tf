858fc0b0a5efd66b0abad89209a3f25f695f1df2
maar <kovalbogdan73@gmail.com>
2025-04-28 22:15:14 +0300

    12

--- route53.tf
+++ route53.tf
@@ -5,11 +5,8 @@ resource "aws_route53_zone" "zone_53" {
 resource "aws_route53_record" "cname_record" {
   zone_id = aws_route53_zone.zone_53.zone_id
   name    = "app.kaashntr.pp.ua"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.nlb.dns_name
-    zone_id                = aws_lb.nlb.zone_id
-    evaluate_target_health = true
-  }
+  
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.nlb.nlb.dns_name]
 }
