8fb1d9c606be3a76b6588956608ce3994a2a3c1f
maar <kovalbogdan73@gmail.com>
2025-05-02 21:19:37 +0300

    1

--- sg.tf
+++ sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "ec2" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "31.128.191.250/32"
+    cidr_blocks = ["31.128.191.250/32"]
   }
   egress {
     from_port   = 0
